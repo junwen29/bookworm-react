7a8967c67c36ac70c472f7e517542bd58dcaa23b
junwen29 <junwen2992@gmail.com>
2018-03-05 15:49:37 +0800

    Make async request to server and render server validation errors

--- src/components/forms/LoginForm.js
+++ src/components/forms/LoginForm.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react';
-import { Form, Button } from 'semantic-ui-react';
+import { Form, Button, Message } from 'semantic-ui-react';
 import Validator from 'validator';
 import PropTypes from 'prop-types';
 import InlineError from '../messages/InlineError';
@@ -22,24 +22,30 @@ class LoginForm extends Component {
         const errors = this.validate(this.state.data);
         this.setState({ errors });
 
-        if(Object.keys(errors).length === 0 ){
-            this.props.submit(this.state.data);
+        if (Object.keys(errors).length === 0) {
+            this.setState({ loading: true });
+            this.props.submit(this.state.data)
+                .catch(err => this.setState({ errors: err.response.data.errors, loading: false }));
         }
     }
 
     validate = (data) => {
         const errors = {};
-        if(!Validator.isEmail(data.email)) errors.email = "Invalid email";
+        if (!Validator.isEmail(data.email)) errors.email = "Invalid email";
         if (!data.password) errors.password = "Can't be blank";
 
         return errors;
     }
 
     render() {
-        const { data, errors } = this.state;
+        const { data, errors, loading } = this.state;
 
         return (
-            <Form onSubmit={this.onSubmit}>
+            <Form onSubmit={this.onSubmit} loading={loading}>
+                {errors.global && <Message negative>
+                    <Message.Header>Something went wrong</Message.Header>
+                    <p>{errors.global}</p>
+                </Message>}
                 <Form.Field error={!!errors.email}>
                     <label htmlFor="email">Email</label>
                     <input
@@ -50,7 +56,7 @@ class LoginForm extends Component {
                         value={data.email}
                         onChange={this.onChange}
                     />
-                    {errors.email && <InlineError text={errors.email}/>}
+                    {errors.email && <InlineError text={errors.email} />}
                 </Form.Field>
                 <Form.Field error={!!errors.password}>
                     <label htmlFor="password">Password</label>
@@ -62,8 +68,8 @@ class LoginForm extends Component {
                         value={data.password}
                         onChange={this.onChange}
                     />
-                    {errors.password && <InlineError text={errors.password}/>}
-                    
+                    {errors.password && <InlineError text={errors.password} />}
+
                 </Form.Field>
                 <Button primary>Login</Button>
             </Form>
